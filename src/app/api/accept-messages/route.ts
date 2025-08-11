c9a776e118c25fced66cae7c7199110acb24ddc5
SurajKale01 <surajkale9140@gmail.com>
2025-08-11 23:25:45 +0530

    feat: update layout and api routes

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,108 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: `Not authenticated`,
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: `unable to find user to update message acceptance status`,
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: `Message acceptance status updated successfully`,
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(`Error updating message acceptance status: ${error}`);
+    return Response.json(
+      {
+        success: false,
+        message: `Error updating message acceptance status`,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!session || !user) {
+    return Response.json(
+      {
+        success: false,
+        message: `Not authenticated`,
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const foundUser = await UserModel.findById(user._id);
+
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: `User not found`,
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(`Error retriving message acceptance status: ${error}`);
+    return Response.json(
+      {
+        success: false,
+        message: `Error retriving message acceptance status`,
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
       return Response.json(
         {
           success: false,
-          message: `Username is alredy taken`,
+          message: "Username is already taken",
         },
         { status: 400 }
       );
@@ -32,7 +32,7 @@ export async function POST(request: Request) {
         return Response.json(
           {
             success: false,
-            message: `User already exists with this email`,
+            message: "User already exists with this email",
           },
           { status: 400 }
         );
@@ -55,8 +55,8 @@ export async function POST(request: Request) {
         verifyCode,
         verifyCodeExpiry: expiryDate,
         isVerified: false,
-        isAcceptingMessage: true,
-        message: [],
+        isAcceptingMessages: true,
+        messages: [],
       });
 
       await newUser.save();
@@ -81,16 +81,16 @@ export async function POST(request: Request) {
     return Response.json(
       {
         success: true,
-        message: `User registered successfully. Please verify your account`,
+        message: "User registered successfully. Please verify your account.",
       },
       { status: 201 }
     );
   } catch (error) {
-    console.error(`Error while registering user ${error}`);
+    console.error("Error registering user:", error);
     return Response.json(
       {
         success: false,
-        message: `Error while registering user`,
+        message: "Error registering user",
       },
       { status: 500 }
     );
