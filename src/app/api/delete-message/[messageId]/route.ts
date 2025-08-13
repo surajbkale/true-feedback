758dc5c713db0c8a6d30278f5497f8a98be0b59b
SurajKale01 <surajkalecsit@gmail.com>
2025-08-13 17:47:18 +0530

    add delete-message api and dashboard component

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { User } from "next-auth";
+import UserModel from "@/model/User";
+import { _success } from "zod/v4/core";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageId: string } }
+) {
+  const messageId = params.messageId;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const _user: User = session?.user;
+
+  if (!session || !_user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: _user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json(
+        { success: false, message: "Message not found or already deleted" },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(`Error deleting message: ${error}`);
+    return Response.json(
+      { success: false, message: "Error deleting message" },
+      { status: 500 }
+    );
+  }
+}
